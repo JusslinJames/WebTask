9a3790e58668e6a48ddf1de5675f6c7c0c5fc574
Jusslin James <jusslingeorge@gmail.com>
2020-07-19 11:58:47 +0530

    Completed

--- Calendar.php
+++ Calendar.php
@@ -4,20 +4,17 @@
 <html>
 <head>
 	<title>Calender</title>
-	<link rel="stylesheet" type="text/css" href="Calendar.css">	
+	<link rel="stylesheet" type="text/css" href="Calendar.css">		
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
 
-    
-<div class="container">
 
-	<div class="btnclass">
-			<a href="index.html" class="btn btn-success mb-3 mx-3" role="button">Create Event</a>
-		<!-- <a href="DisplayEvent.php" class="btn btn-success mb-3" role="button">View / Delete Event</a> -->
-	</div>
+<div class="container c">
+
+	
 
 	<div class="calender">
 		<div class="months">
@@ -53,19 +50,20 @@
 
 		</div>
 		
+		<div class="btnclass text-center">
+			<a href="index.html" class="btn btn-success mb-3 mx-3" role="button">Create Event</a>
+		<!-- <a href="DisplayEvent.php" class="btn btn-success mb-3" role="button">View / Delete Event</a> -->
+		</div>  
+
 	</div>
 
-    
-</div>  
-
-
-
-
+<!-- try box -->
+<div class="bx" style="height:auto; width:600px; border:1px solid black; margin-left:20px;">
 <div class="content">
 
 <table class="table table-striped">
   <thead>
-    <tr class="bg-dark text-white">
+    <tr style="background-color:black; color:white; text-align:center;">
       <th scope="col">Sr.</th>
       <!-- <th scope="col">Id</th> -->
       <th scope="col">Event Date</th>
@@ -80,16 +78,31 @@
 </table>
 
 </div>
+</div>
+<!-- try ends -->
+
+
+</div>  
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 <script type="text/javascript" src="Calendar.js"></script>
-<script src="https://code.jquery.com/jquery-3.1.1.min.js">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	</body>
 
 
 </html>
